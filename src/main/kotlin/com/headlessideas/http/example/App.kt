e42d9913d175c330152fb4e8b85f8e32e8dd584b
Simon Bakkevig <simon.nandi@gmail.com>
2017-07-25 23:00:59 +0200

    Switches from command line config to using .properties file for configuring the server

--- src/main/kotlin/com/headlessideas/http/example/App.kt
+++ src/main/kotlin/com/headlessideas/http/example/App.kt
@@ -2,23 +2,59 @@ package com.headlessideas.http.example
 
 import com.headlessideas.http.Server
 import com.xenomachina.argparser.ArgParser
-import com.xenomachina.argparser.default
 import com.xenomachina.argparser.mainBody
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.*
+import kotlin.system.exitProcess
+
 
 fun main(args: Array<String>): Unit = mainBody("klutch") {
-
     Args(ArgParser(args)).run {
-        val server = Server(rootDir, port)
-        server.serve()
+
+        val message = checkConfig(configFile)
+
+        if (message.isNotEmpty()) {
+            println(message)
+            if (check) {
+                exitProcess(0)
+            }
+            exitProcess(1)
+        }
+
+        if (!check) {
+            val root = configFile.getProperty("server.root").toPath()
+            val port = configFile.getProperty("server.port").toInt()
+
+            val server = Server(root, port)
+            server.serve()
+        } else {
+            println("OK")
+        }
     }
 
+}
+
+fun checkConfig(prop: Properties): String {
+    var message = ""
+
+    if (prop.getProperty("server.root") == null) {
+        message += "server.root is missing\r\n"
+    }
+    if (prop.getProperty("server.port") == null) {
+        message += "server.port is missing\r\n"
+    }
+
+    return message
 }
 
 fun String.toPath(): Path = Paths.get(this)
 
 class Args(parser: ArgParser) {
-    val port by parser.storing("-p", "--port", help = "Port number, default 9000", argName = "PORT_NUMBER") { toInt() }.default(9000)
-    val rootDir by parser.positional("ROOT_DIR", help = "The document root for the web server") { toPath() }
+    val check by parser.flagging("-c", "--check", help = "Checks the provided configuration file")
+    val configFile by parser.positional("CONFIG", help = "The configuration file for the web server") {
+        val prop = Properties()
+        prop.load(toPath().toFile().inputStream())
+        prop
+    }
 }
